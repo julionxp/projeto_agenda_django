40ff2343132a2ed364e0ad38ed0bfb5f5ab748d0
Julio Cezar <juliondiniz@gmail.com>
2026-05-15 12:36:09 -0300

    Usando a classe Paginator para paginação no Django

---
+++ contact/views/contact_views.py
@@ -0,0 +1,83 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
+from django.http import Http404
+from contact.models import Contact
+from django.core.paginator import Paginator
+
+# Create your views here.
+def index(request):
+    contacts = Contact.objects \
+        .filter(show=True)\
+        .order_by('-id')
+    
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        'page_obj': page_obj,
+        'site_title': 'Contatos - '  
+    }
+
+    return render(
+        request,
+        'contact/index.html', context
+    )
+
+
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+    print(search_value)
+
+    if search_value == '':
+        return redirect('contact:index')
+    
+    print(search_value)
+
+    contacts = Contact.objects \
+        .filter(show=True)\
+        .filter(
+            Q(first_name__icontains=search_value) | 
+            Q(last_name__icontains=search_value) | 
+            Q(phone__icontains=search_value) | 
+            Q(email__icontains=search_value)
+        )\
+        .order_by('-id')
+    
+    paginator = Paginator(contacts, 10)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    
+    print(contacts.query)
+
+    context = {
+        'page_obj': page_obj,
+        'site_title': 'Search - ',
+        'search_value': search_value
+    }
+
+    return render(
+        request,
+        'contact/index.html', context
+    )
+
+
+
+
+
+# Create your views here.
+def contact(request, contact_id):
+    # single_contact = Contact.objects.filter(pk=contact_id).first()
+    single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
+
+    contact_name = f'{single_contact.first_name} {single_contact.last_name} - '
+
+    context = {
+        'contact': single_contact,
+        'site_title': contact_name,
+    }
+
+    return render(
+        request,
+        'contact/contact.html', context
+    )
